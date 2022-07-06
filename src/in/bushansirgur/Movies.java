a3581eccc1b232d9b5440d8f616219b83a592581
Bushan SC <bushan1992@zohomail.in>
2022-07-06 23:59:03 +0530

    lesson 01

---
+++ src/in/bushansirgur/Movies.java
@@ -0,0 +1,11 @@
+package in.bushansirgur;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Movies {
+	
+	public List<String> getMovies() {
+		return Collections.emptyList();
+	}
+}
---
+++ test/in/bushansirgur/MoviesSpec.java
@@ -0,0 +1,27 @@
+package in.bushansirgur;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class MoviesSpec {
+
+	@Test
+	public void moviesEmptyWhenNoMoviesAdded() {
+		//Arrange -> setup the data 
+		Movies movies = new Movies();
+		
+		//Act -> call the method
+		List<String> list = movies.getMovies();
+		
+		//Assert -> evaualted the condition
+		//success scenario
+		assertTrue(list.isEmpty(), () -> "Movies should be empty");
+		//failure scenario
+		assertFalse(list.isEmpty(), () -> "Movies should be empty");
+	}
+
+}
